7397dfe146c7ca6bbb58e9c382498069178ba764
Suraj Subramanian <5676233+suraj813@users.noreply.github.com>
2023-01-18 07:37:33 -0800

    Relicense MoCo under MIT (#135)
    Summary:
    Updates headers and LICENSE file to be MIT (more permissive than CC-BY-NC)
    
    Pull Request resolved: https://github.com/facebookresearch/moco/pull/135
    
    Reviewed By: haooooooqi, endernewton
    
    Differential Revision: D42372191
    
    Pulled By: suraj813
    
    fbshipit-source-id: 6c217b452008a3231f747c440162c5c186e18fde

--- main_moco.py
+++ main_moco.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 
 import argparse
 import builtins
