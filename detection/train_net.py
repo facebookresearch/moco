b8ae0d98d2ce543a96d2254d3875e9f7de10a6fe
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2025-09-30 09:16:34 -0700

    Add missing Pyre mode headers] [batch:28/N] [shard:14/N] [A] [A] [B]
    Differential Revision: D83563844
    
    fbshipit-source-id: 60917699affae93cf99878107355c38f43edd309

--- detection/train_net.py
+++ detection/train_net.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
+# pyre-unsafe
+
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
